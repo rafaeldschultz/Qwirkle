45c250f7bf445a738bda58185d59eef73d0c8e53
rafaelschultz <rafael.schultz@usp.br>
2020-07-12 15:31:23 -0300

    Corrigido bug para unir linhas e colunas já existentes

--- game.c
+++ game.c
@@ -109,6 +109,68 @@ short verifyDuplicates(Game *g, Block b, int lin, int col){
     return 1;
 }
 
+short verifyDuplicatesJoinedVertical(Game *g, int lin, int col){
+    short i = 1;
+
+    Block b[10]; //5 acima e 5 abaixo da peça
+
+    //Acima
+    while((lin - i >= 0) && (g->board[lin - i][col].letter != '\0')){
+        b[i - 1] = g->board[lin - i][col];
+        i++;
+    }
+
+    short j = i - 1;
+    i = 1;
+
+    //Abaixo
+    while((lin + i <= g->max_lin) && (g->board[lin + i][col].letter != '\0')){
+        b[j] = g->board[lin + i][col];
+        j++;
+        i++;
+    }
+
+    for(i = 0; i < j; i++){
+        for(short k = i + 1; k < j; k++){
+            if((b[i].letter == b[k].letter) && (b[i].number == b[k].number)){
+                return 0;
+            }
+        } 
+    }
+    return 1;
+}
+
+short verifyDuplicatesJoinedHorizontal(Game *g, int lin, int col){
+    short i = 1;
+
+    Block b[10]; //5 acima e 5 abaixo da peça
+
+    //A esquerda
+    while((col - i >= 0) && (g->board[lin][col - i].letter != '\0')){
+        b[i - 1] = g->board[lin][col - i];
+        i++;
+    }
+
+    short j = i - 1;
+    i = 1;
+
+    //A direita
+    while((col + i <= g->max_col) && (g->board[lin][col + i].letter != '\0')){
+        b[j] = g->board[lin][col + i];
+        j++;
+        i++;
+    }
+
+    for(i = 0; i < j; i++){
+        for(short k = i + 1; k < j; k++){
+            if((b[i].letter == b[k].letter) && (b[i].number == b[k].number)){
+                return 0;
+            }
+        } 
+    }
+    return 1;
+}
+
 short verifyMoviment(Game *g, Block b, int lin, int col, short *f_lin, short *f_col, short *lt){
     //0: indefinido; 1: letra; 2: numero
 
@@ -183,147 +245,161 @@ short verifyMoviment(Game *g, Block b, int lin, int col, short *f_lin, short *f_
         empty++;
     }
 
-    if(empty < 4){
-        if(isPossible_up == 0){                         //Nao vazio e existente
-            if(up.relation.vertical == 0){
-                if(b.letter == up.letter){
-                    b.relation.vertical = 1;
-                    up.relation.vertical = 1;
-                    isPossible_up = 1;
-                } else if(b.number == up.number) {
-                    b.relation.vertical = 2;
-                    up.relation.horizontal = 2;
-                    isPossible_up = 1;
-                }
-            } else if(up.relation.vertical == 1){
-                if(b.letter == up.letter){
-                    b.relation.vertical = 1;
-                    isPossible_up = 1;
-                }
-            } else {
-                if(b.number == up.number){
-                    b.relation.vertical = 2;
-                    isPossible_up = 1;
-                }
-            }
-        }
+    short duplicatedVertical = 1, duplicatedHorizontal = 1;
 
-        if(isPossible_down == 0){                         //Nao vazio e existente
-            if(down.relation.vertical == 0){
-                if(b.letter == down.letter){
-                    if(b.relation.vertical < 2){
+    if(isPossible_down == 0 && isPossible_up == 0){
+       duplicatedVertical = verifyDuplicatesJoinedVertical(g, lin, col);
+    }
+
+    if(isPossible_left == 0 && isPossible_right == 0){
+        duplicatedHorizontal = verifyDuplicatesJoinedHorizontal(g, lin, col);
+    }
+
+    if(duplicatedVertical && duplicatedHorizontal){
+        if(empty < 4){
+            if(isPossible_up == 0){                         //Nao vazio e existente
+                if(up.relation.vertical == 0){
+                    if(b.letter == up.letter){
                         b.relation.vertical = 1;
-                        down.relation.vertical = 1;
-                        isPossible_down = 1;
-                    } 
-                } else if(b.number == down.number) {
-                    if(b.relation.vertical != 1){
+                        up.relation.vertical = 1;
+                        isPossible_up = 1;
+                    } else if(b.number == up.number) {
                         b.relation.vertical = 2;
-                        down.relation.horizontal = 2;
-                        isPossible_down = 1;
+                        up.relation.horizontal = 2;
+                        isPossible_up = 1;
                     }
-                }
-            } else if(down.relation.vertical == 1){
-                if(b.letter == down.letter){
-                    if(b.relation.vertical < 2){
+                } else if(up.relation.vertical == 1){
+                    if(b.letter == up.letter){
                         b.relation.vertical = 1;
-                        isPossible_down = 1;
+                        isPossible_up = 1;
                     }
-                }
-            } else {
-                if(b.number == down.number){
-                    if(b.relation.vertical != 1){
+                } else {
+                    if(b.number == up.number){
                         b.relation.vertical = 2;
-                        isPossible_down = 1;
+                        isPossible_up = 1;
                     }
                 }
             }
-        }
 
-        if(isPossible_left == 0){                         //Nao vazio e existente
-            if(left.relation.horizontal == 0){
-                if(b.letter == left.letter){
-                    b.relation.horizontal = 1;
-                    left.relation.horizontal = 1;
-                    isPossible_left = 1;
-                } else if(b.number == left.number) {
-                    b.relation.horizontal = 2;
-                    left.relation.horizontal = 2;
-                    isPossible_left = 1;
-                }
-            } else if(left.relation.horizontal == 1){
-                if(b.letter == left.letter){
-                    b.relation.horizontal = 1;
-                    isPossible_left = 1;
-                }
-            } else {
-                if(b.number == left.number){
-                    b.relation.horizontal = 2;
-                    isPossible_left = 1;
+            if(isPossible_down == 0){                         //Nao vazio e existente
+                if(down.relation.vertical == 0){
+                    if(b.letter == down.letter){
+                        if(b.relation.vertical < 2){
+                            b.relation.vertical = 1;
+                            down.relation.vertical = 1;
+                            isPossible_down = 1;
+                        } 
+                    } else if(b.number == down.number) {
+                        if(b.relation.vertical != 1){
+                            b.relation.vertical = 2;
+                            down.relation.horizontal = 2;
+                            isPossible_down = 1;
+                        }
+                    }
+                } else if(down.relation.vertical == 1){
+                    if(b.letter == down.letter){
+                        if(b.relation.vertical < 2){
+                            b.relation.vertical = 1;
+                            isPossible_down = 1;
+                        }
+                    }
+                } else {
+                    if(b.number == down.number){
+                        if(b.relation.vertical != 1){
+                            b.relation.vertical = 2;
+                            isPossible_down = 1;
+                        }
+                    }
                 }
             }
-        }
 
-        if(isPossible_right == 0){                         //Nao vazio e existente
-            if(right.relation.horizontal == 0){
-                if(b.letter == right.letter){
-                    if(b.relation.horizontal < 2){
+            if(isPossible_left == 0){                         //Nao vazio e existente
+                if(left.relation.horizontal == 0){
+                    if(b.letter == left.letter){
                         b.relation.horizontal = 1;
-                        right.relation.horizontal = 1;
-                        isPossible_right = 1;
-                    } 
-                } else if(b.number == right.number) {
-                    if(b.relation.horizontal != 1){
+                        left.relation.horizontal = 1;
+                        isPossible_left = 1;
+                    } else if(b.number == left.number) {
                         b.relation.horizontal = 2;
-                        right.relation.horizontal = 2;
-                        isPossible_right = 1;
+                        left.relation.horizontal = 2;
+                        isPossible_left = 1;
                     }
-                }
-            } else if(right.relation.horizontal == 1){
-                if(b.letter == right.letter){
-                    if(b.relation.horizontal < 2){
+                } else if(left.relation.horizontal == 1){
+                    if(b.letter == left.letter){
                         b.relation.horizontal = 1;
-                        isPossible_right = 1;
+                        isPossible_left = 1;
                     }
-                }
-            } else {
-                if(b.number == right.number){
-                    if(b.relation.horizontal != 1){
+                } else {
+                    if(b.number == left.number){
                         b.relation.horizontal = 2;
-                        isPossible_right = 1;
+                        isPossible_left = 1;
                     }
                 }
             }
-        }
 
-        if(isPossible_down == 1 && isPossible_up == 1 && isPossible_right == 1 && isPossible_left == 1){
-            if(verifyDuplicates(g, b, lin, col)){
-                if(lin > 0){
-                    g->board[lin - 1][col].relation.vertical = up.relation.vertical;
-                }
-                if(lin < g->max_lin) {
-                    g->board[lin + 1][col].relation.vertical = down.relation.vertical;
-                }
-                if(col > 0) {
-                    g->board[lin][col - 1].relation.horizontal = left.relation.horizontal;
-                }
-                if(col < g->max_col) {
-                    g->board[lin][col + 1].relation.horizontal = right.relation.horizontal;
-                }
-                
-                if(*f_col == -1 && *f_lin == -1){
-                    *f_col = col;
-                    *f_lin = lin;
-                } else if(*lt == 0){
-                    if(*f_lin == lin){
-                        *lt = 1;
-                    } else {
-                        *lt = 2;
+            if(isPossible_right == 0){                         //Nao vazio e existente
+                if(right.relation.horizontal == 0){
+                    if(b.letter == right.letter){
+                        if(b.relation.horizontal < 2){
+                            b.relation.horizontal = 1;
+                            right.relation.horizontal = 1;
+                            isPossible_right = 1;
+                        } 
+                    } else if(b.number == right.number) {
+                        if(b.relation.horizontal != 1){
+                            b.relation.horizontal = 2;
+                            right.relation.horizontal = 2;
+                            isPossible_right = 1;
+                        }
+                    }
+                } else if(right.relation.horizontal == 1){
+                    if(b.letter == right.letter){
+                        if(b.relation.horizontal < 2){
+                            b.relation.horizontal = 1;
+                            isPossible_right = 1;
+                        }
+                    }
+                } else {
+                    if(b.number == right.number){
+                        if(b.relation.horizontal != 1){
+                            b.relation.horizontal = 2;
+                            isPossible_right = 1;
+                        }
                     }
                 }
+            }
 
-                makeMoviment(g, b, lin, col, f_lin, f_col);
-                return 1;
+            if(isPossible_down == 1 && isPossible_up == 1 && isPossible_right == 1 && isPossible_left == 1){
+                if(verifyDuplicates(g, b, lin, col)){
+                    if(lin > 0){
+                        g->board[lin - 1][col].relation.vertical = up.relation.vertical;
+                    }
+                    if(lin < g->max_lin) {
+                        g->board[lin + 1][col].relation.vertical = down.relation.vertical;
+                    }
+                    if(col > 0) {
+                        g->board[lin][col - 1].relation.horizontal = left.relation.horizontal;
+                    }
+                    if(col < g->max_col) {
+                        g->board[lin][col + 1].relation.horizontal = right.relation.horizontal;
+                    }
+                    
+                    if(*f_col == -1 && *f_lin == -1){
+                        *f_col = col;
+                        *f_lin = lin;
+                    } else if(*lt == 0){
+                        if(*f_lin == lin){
+                            *lt = 1;
+                        } else {
+                            *lt = 2;
+                        }
+                    }
+
+                    makeMoviment(g, b, lin, col, f_lin, f_col);
+                    return 1;
+                } else {
+                    invalidMove();
+                }
             } else {
                 invalidMove();
             }
